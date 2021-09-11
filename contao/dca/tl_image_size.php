d35d2975933be754c21a0c8bea0ef4cae1765812
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2021-09-11 23:47:38 +0200

    Fix the unique definition

--- contao/dca/tl_image_size.php
+++ contao/dca/tl_image_size.php
@@ -10,6 +10,6 @@ declare(strict_types=1);
  * @license MIT
  */
 
-$GLOBALS['TL_DCA']['tl_theme']['config']['sql']['keys']['alias'] = 'unique';
+$GLOBALS['TL_DCA']['tl_theme']['config']['sql']['keys']['pid,alias'] = 'unique';
 
 $GLOBALS['TL_DCA']['tl_image_size']['fields']['alias']['sql'] = 'varchar(128) default NULL';
