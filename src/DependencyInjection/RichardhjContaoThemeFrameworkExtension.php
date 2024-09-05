093c271c38b9828e07678cee5519ec91c6407af2
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2024-09-06 00:22:13 +0200

    Fix regression

--- src/DependencyInjection/RichardhjContaoThemeFrameworkExtension.php
+++ src/DependencyInjection/RichardhjContaoThemeFrameworkExtension.php
@@ -20,6 +20,11 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class RichardhjContaoThemeFrameworkExtension extends Extension
 {
+    public function getAlias(): string
+    {
+        return 'contao_theme_framework';
+    }
+
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../../config'));
