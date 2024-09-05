cd6e4bec6956ba5d32943347beb0fff5215e3cd3
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2024-09-06 00:23:55 +0200

    Fix regression

--- src/RichardhjContaoThemeFrameworkBundle.php
+++ src/RichardhjContaoThemeFrameworkBundle.php
@@ -13,7 +13,9 @@ declare(strict_types=1);
 namespace Richardhj\ContaoThemeFramework;
 
 use Richardhj\ContaoThemeFramework\DependencyInjection\CompilerPass\AddAssetsPackagesPass;
+use Richardhj\ContaoThemeFramework\DependencyInjection\RichardhjContaoThemeFrameworkExtension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class RichardhjContaoThemeFrameworkBundle extends Bundle
@@ -21,6 +23,11 @@ class RichardhjContaoThemeFrameworkBundle extends Bundle
     public function getPath(): string
     {
         return \dirname(__DIR__);
+    }
+
+    public function getContainerExtension(): ?ExtensionInterface
+    {
+        return new RichardhjContaoThemeFrameworkExtension();
     }
 
     public function build(ContainerBuilder $container)
